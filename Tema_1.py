b05318dddaa85df0c74c2b1e925c25be428446c1
Drăghici Andrei <35525357+Sekushy@users.noreply.github.com>
2019-05-27 15:09:44 +0300

    Made minor improvements to the code style

--- Tema_1.py
+++ Tema_1.py
@@ -4,23 +4,21 @@ from random import randint
 import csv
 import Sorting
 
-def runSortingAlgorithm(sortingColumn, sortingAlgorithmName):
+def runSortingAlgorithm(sortingAlgorithmName):
     # Create a temporary array that takes on the value of the randomized array each time so that the execution times remain constant
     temporaryArray = randomizedArray
-
     print('------Begin ' + sortingAlgorithmName + '------')
     execution = 'Sorting.{}(temporaryArray)'.format((sortingAlgorithmName[0].lower() + sortingAlgorithmName[1:]).replace(' ', ''))
-
     startExecutionTime = time.time()
     exec(execution)
     finalExecutionTime = float('{0:.4f}'.format(time.time() - startExecutionTime))
-
-    print('Exection time in seconds: {}'.format(finalExecutionTime))
-    sortingColumn.append(finalExecutionTime)
+    print('Exection time in seconds: {}'.format(finalExecutionTime, end='\n'))
+    sortingColumn = '{}'.format((sortingAlgorithmName[0].lower() + sortingAlgorithmName[1:]).replace(' ', '') + 'Column.append(' + str(finalExecutionTime) + ')')
+    exec(sortingColumn)
 
 if __name__ == '__main__':
     sizeOfArray = int(input('Please input the size of your array: '))
-    
+
     # Code that generates the random array of numbers depending on the size
     # given by the user
     randomizedArray = []
@@ -32,18 +30,11 @@ if __name__ == '__main__':
     mergeSortColumn = []
     insertionSortColumn = []
 
-    columnNames = ['Merge Sort', 'Insertion Sort', 'Bubble Sort']
+    columnNames = ['Bubble Sort', 'Merge Sort', 'Insertion Sort']
 
-    for i in range(3):
-        # Merge Algorithm
-        runSortingAlgorithm(mergeSortColumn, 'Merge Sort')
-
-        # Insertion Algorithm
-        runSortingAlgorithm(insertionSortColumn, 'Insertion Sort')
-        
-        # Bubble Algorithm
-        runSortingAlgorithm(bubbleSortColumn, 'Bubble Sort')
-        
+    for i in range(9):
+        j = i % len(columnNames)
+        runSortingAlgorithm(str(columnNames[j]))
         i = i + 1
 
 with open('startExecutionTimes_' + str(sizeOfArray) + '.csv', 'w', newline='') as csvfile:
